6f2682fea9b9392b266aa986812b4eebcddd51ab
Lemuel <lemuelcco@gmail.com>
2018-07-24 11:19:34 +0100

    add Realm models for user information and attendance

---
+++ app/src/main/java/lemuel/lemubit/com/biometricattendance/model/attendanceDb.java
@@ -0,0 +1,42 @@
+package lemuel.lemubit.com.biometricattendance.model;
+
+import io.realm.RealmObject;
+
+public class attendanceDb extends RealmObject {
+    private int id;
+    private String time;
+    private String date;
+    private String action;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
---
+++ app/src/main/java/lemuel/lemubit/com/biometricattendance/model/userInformationDb.java
@@ -0,0 +1,182 @@
+package lemuel.lemubit.com.biometricattendance.model;
+
+import io.realm.RealmObject;
+
+public class userInformationDb extends RealmObject {
+    private int id;
+    private byte [] userPhoto;
+    private String clockedState;
+
+    private String firstName;
+    private String lastName;
+    private String sex;
+    private String dateOfBirth;
+    private String email;
+    private String phoneNumber;
+
+    private int rightThumb;
+    private int rightIndex;
+    private int rightMiddle;
+    private int rightRing;
+    private int rightPinky;
+
+    private int leftThumb;
+    private int leftIndex;
+    private int leftMiddle;
+    private int leftRing;
+    private int leftPinky;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte[] getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(byte[] userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+
+    public String getClockedState() {
+        return clockedState;
+    }
+
+    public void setClockedState(String clockedState) {
+        this.clockedState = clockedState;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getRightThumb() {
+        return rightThumb;
+    }
+
+    public void setRightThumb(int rightThumb) {
+        this.rightThumb = rightThumb;
+    }
+
+    public int getRightIndex() {
+        return rightIndex;
+    }
+
+    public void setRightIndex(int rightIndex) {
+        this.rightIndex = rightIndex;
+    }
+
+    public int getRightMiddle() {
+        return rightMiddle;
+    }
+
+    public void setRightMiddle(int rightMiddle) {
+        this.rightMiddle = rightMiddle;
+    }
+
+    public int getRightRing() {
+        return rightRing;
+    }
+
+    public void setRightRing(int rightRing) {
+        this.rightRing = rightRing;
+    }
+
+    public int getRightPinky() {
+        return rightPinky;
+    }
+
+    public void setRightPinky(int rightPinky) {
+        this.rightPinky = rightPinky;
+    }
+
+    public int getLeftThumb() {
+        return leftThumb;
+    }
+
+    public void setLeftThumb(int leftThumb) {
+        this.leftThumb = leftThumb;
+    }
+
+    public int getLeftIndex() {
+        return leftIndex;
+    }
+
+    public void setLeftIndex(int leftIndex) {
+        this.leftIndex = leftIndex;
+    }
+
+    public int getLeftMiddle() {
+        return leftMiddle;
+    }
+
+    public void setLeftMiddle(int leftMiddle) {
+        this.leftMiddle = leftMiddle;
+    }
+
+    public int getLeftRing() {
+        return leftRing;
+    }
+
+    public void setLeftRing(int leftRing) {
+        this.leftRing = leftRing;
+    }
+
+    public int getLeftPinky() {
+        return leftPinky;
+    }
+
+    public void setLeftPinky(int leftPinky) {
+        this.leftPinky = leftPinky;
+    }
+}
